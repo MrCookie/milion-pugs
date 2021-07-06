0b795fb97ef8e04fe357078ea52a27b21c412ba2
Hubert Rzepinski <hubert.rzepinski@gmail.com>
2021-07-07 00:29:36 +0200

    feat: Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import './styles/global.scss';
+import { ImageVewer } from './pages/ImageViewer/ImageViewer';
+import { Favourites } from './pages/Favourites/Favourites';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ImageVewer} />
+        <Route exact path="/zapisane" component={Favourites} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
